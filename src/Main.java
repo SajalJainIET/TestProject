9b16a726986b8e509aad9be42376f3d949021711
Sajal Jain <sajaljain.iet@gmail.com>
2026-04-28 16:09:36 +0530

    m3 added

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void m2() {
         System.out.println("m2");
     }
+    public static void m2() {
+        System.out.println("m3");
+    }
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
